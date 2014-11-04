7542ef021e7de864cbc4c219f5826dd27375fef4
thatnerdjack <jackjyro@gmail.com>
2014-11-04 13:39:41 -0500

    non-working w/ hard stuff

--- src/com/github/thatnerdjack/multiples/Multiples.java
+++ src/com/github/thatnerdjack/multiples/Multiples.java
@@ -1,5 +1,7 @@
 package com.github.thatnerdjack.multiples;
 
+import java.util.ArrayList;
+
 /**
  * Created by block7 on 11/4/14.
  */
@@ -19,11 +21,42 @@ public class Multiples {
         System.out.println("multiples of " + a + " up to " + b + " are: " + multiples);
     }
 
+    public static ArrayList findMultiplesTo(int a, int b) {
+        int latestAnswer = a;
+        ArrayList<Integer> multipleArray = new ArrayList<Integer>();
+
+        for (int i = 1; latestAnswer <= b; i++) {
+            latestAnswer = a * i;
+            multipleArray.add(latestAnswer);
+            if (((i + 1) * a) > b) {
+                break;
+            }
+        }
+        return multipleArray;
+    }
+
+    public static void leastCommonMultiple(int a, int b, int c) {
+        ArrayList<Integer> num1Array = findMultiplesTo(a, c);
+        ArrayList<Integer> num2Array = findMultiplesTo(b, c);
+
+        for (int i = 0; i < num1Array.size(); i++) {
+            for (int i2 = 0; i2 < num2Array.size(); i++) {
+                int numTest1 = num1Array.get(i);
+                int numTest2 = num2Array.get(i2);
+                if (numTest1 == numTest2) {
+                    System.out.println("The least common multiple of " + a + " and " + b + " is " + numTest1);
+                    break;
+                }
+            }
+        }
+    }
+
     public static void main(String args[]) {
         printMultiplesTo(2, 10);
         printMultiplesTo(12, 100);
         printMultiplesTo(1, 7);
         printMultiplesTo(8, 2);
+        leastCommonMultiple(12, 100, 1000);
     }
 
 }
